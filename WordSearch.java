290d82f781623fb60c2eba3f223e388a83dc27cc
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-11-16 09:57:07 -0500

    Making progress, fixed bug.

--- WordSearch.java
+++ WordSearch.java
@@ -95,20 +95,6 @@ public WordSearch (filename, rows, coles, seed boolean key) {
         return s;
       }
 
-    //Two Methods
-    /**Attempts to add a given word to the specified position of the WordGrid.
-   *The word is added in the direction rowIncrement,colIncrement
-   *Words must have a corresponding letter to match any letters that it overlaps.
-   *
-   *@param word is any text to be added to the word grid.
-   *@param row is the vertical locaiton of where you want the word to start.
-   *@param col is the horizontal location of where you want the word to start.
-   *@param rowIncrement is -1,0, or 1 and represents the displacement of each letter in the row direction
-   *@param colIncrement is -1,0, or 1 and represents the displacement of each letter in the col direction
-   *@return true when: the word is added successfully.
-   *        false when: the word doesn't fit, OR  rowchange and colchange are both 0,
-   *        OR there are overlapping letters that do not match
-   */
 private boolean addWord(int row, int col, String word, int rowIncrement, int colIncrement) {
 
 if ( (colIncrement = 0 && rowIncrement = 0 )  {
@@ -132,7 +118,7 @@ if ( (colIncrement = 0 && rowIncrement = 0 )  {
       }
       //checks col.
 for (int i = 0; i < word.length(); i++) {
-  if (data[row][col] = '_' || data[row][col] == word.charAt(i)) {
+  if (data[row][col] == '_' || data[row][col] == word.charAt(i)) {
     data[row][col] = word.charAt(i);
     row += rowIncrement;
     col += colIncrement;
@@ -145,36 +131,6 @@ for (int i = 0; i < word.length(); i++) {
 }
 
 private void addAllWords() {
-    if (addWord(Random word, row, col, Random colIncrement, Random rowIncrement)) {
-      //add the word to the place where all words are........
-    }
-    else {
-      if (Word = )
-
-    }
-
-    }
-
-  }
-
-}
-
-
-  /*-Attempt to add all of the words from the wordsToAdd list using the following algorithm:
-    1-Choose a random word, and a random direction (rowIncrement/colIncrement)
-    2-Trying to add that word to different starting positions until:
-        --you successfully add the word,
-        --or you run out of positional tries*
-      Each positional try only changes the position of the start, not the direction or word.
-      *(Experiment with how many positional tries, maybe 100 is enough, maybe 1000.)
-    3-Repeat this process until you added all of the words, or you tried to add unsuccessfully too many times* in a row. (experiment how many is sufficient to add most of the words)
-
-    4-OPTIONAL
-    Optimize your addAllWords such that:
-    -You do not try to add words to positions that would not fit the word on the board.
-    -Use the rowIncrement/colIncrement to decide what the range of valid row/col should be.
-    e.g.
-    A five letter word that is meant to be added across to the right should not START in the last 4 columns of the board
-*/
+  needAdd = getWord(fileName);
 
   }
